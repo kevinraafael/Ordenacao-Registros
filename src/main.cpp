5c15a10da8de43dfc23c9ecb3816995996cc40c8
Kevin Francelino <kevin.francelino@redetendencia.com.br>
2021-10-29 12:08:22 -0400

    Finalizado

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,10 @@
 struct Estudante
 {
     int RGA;
-    char nome[101];
+    char nome[101]; // Acrescentei um caractere a mais ,  devido ao /0 no final
     double media;
-};
-// bool compara(bool condition)
-// {
-//     if (condition)
-//     {
-//         return true;
-//     }
-//     return false;
-// }
+}; // Registro do estudante
+
 void merge(Estudante v[], int inicio, int meio, int fim, char op)
 {
     int p1, p2, tamanho, i, j, k;
@@ -23,71 +16,72 @@ void merge(Estudante v[], int inicio, int meio, int fim, char op)
     p2 = meio + 1;
     Estudante temp[tamanho];
 
-    if (temp != NULL)
+    for (i = 0; i < tamanho; i++)
     {
-        for (i = 0; i < tamanho; i++)
+        if (!fim1 && !fim2)
         {
-            if (!fim1 && !fim2)
-            {
-                if (op == 'r')
-                {
-                    if ((v[p1].RGA < v[p2].RGA))
-                    {
-                        temp[i] = v[p1++];
-                    }
-                    else
-                    {
-                        temp[i] = v[p2++];
-                    }
-                }
-                else if (op == 'n')
-                {
-                    if ((strcmp(v[p1].nome, v[p2].nome) < 0))
-                    {
-                        temp[i] = v[p1++];
-                    }
-                    else
-                    {
-                        temp[i] = v[p2++];
-                    }
-                }
-                else if (op == 'm')
-                {
-                    if ((v[p1].media < v[p2].media))
-                    {
-                        temp[i] = v[p1++];
-                    }
-                    else
-                    {
-                        temp[i] = v[p2++];
-                    }
-                }
+            // Nos  3 ifs abaixo realizo as comparacoes de acordo com a operacao solicitada e passo para o meu vetor que esta recebendo os dados ordenados
 
-                if (p1 > meio)
-                {
-                    fim1 = 1; // Verifico se  o vetor acabou
-                }
-                if (p2 > fim)
-                {
-                    fim2 = 1; // Verifico se o vetor acabou
-                }
-            }
-            else
+            if (op == 'r')
             {
-                if (!fim1)
+                if ((v[p1].RGA < v[p2].RGA))
                 {
-                    temp[i] = v[p1++]; // Copio o que sobrou para o meu vetor auxiliar
+                    temp[i] = v[p1++];
                 }
                 else
                 {
-                    temp[i] = v[p2++]; // Copio o que sobrou para o meu vetor auxiliar
+                    temp[i] = v[p2++];
                 }
             }
+            else if (op == 'n')
+            {
+                // Utilizo o strcmp para verificar a ordem lexicografica  , se retornar um valor menor que o
+                // Significa que o conteúdo da primeira string é menor que o da segunda
+                if ((strcmp(v[p1].nome, v[p2].nome) < 0))
+                {
+                    temp[i] = v[p1++];
+                }
+                else
+                {
+                    temp[i] = v[p2++];
+                }
+            }
+            else if (op == 'm')
+            {
+                if ((v[p1].media < v[p2].media))
+                {
+                    temp[i] = v[p1++];
+                }
+                else
+                {
+                    temp[i] = v[p2++];
+                }
+            }
+
+            if (p1 > meio)
+            {
+                fim1 = 1; // Verifico se  o vetor acabou
+            }
+            if (p2 > fim)
+            {
+                fim2 = 1; // Verifico se o vetor acabou
+            }
         }
-        for (j = 0, k = inicio; j < tamanho; j++, k++) // Copio o conteúdo ordenado do vetor auxiliar para o meu original
+        else
         {
-            v[k] = temp[j];
+            if (!fim1)
+            {
+                temp[i] = v[p1++]; // Copio o que sobrou para o meu vetor auxiliar
+            }
+            else
+            {
+                temp[i] = v[p2++]; // Copio o que sobrou para o meu vetor auxiliar
+            }
         }
+    }
+    for (j = 0, k = inicio; j < tamanho; j++, k++) // Copio o conteúdo ordenado do vetor auxiliar para o meu original
+    {
+        v[k] = temp[j];
     }
 }
 
@@ -104,7 +98,7 @@ void mergeSort(Estudante v[], int inicio, int fim, char op)
 }
 
 void ordenaEstudantes(Estudante v[], int n, char op)
-{
+{ // Nessa funcao passo os dados necessarios para meu algoritmo que realizara essa ordenacao - MergeSort
     int inicio, fim;
     inicio = 0;
     fim = n - 1;
@@ -112,7 +106,8 @@ void ordenaEstudantes(Estudante v[], int n, char op)
 }
 void imprimeEstudante(Estudante v[], int k)
 
-{
+{ // Essa condição me auxilia na hora que realizo a busca binária , pois  se eu não encontrar o elemento buscado vai retornar -1
+    // Dessa forma significa que não encontrei o aluno informado , e imprimo com valores null.
     if (k != -1)
         printf("RGA: %d\nNome: %s\nMedia: %.2f\n------------\n", v[k].RGA, v[k].nome, v[k].media);
     else
@@ -212,15 +207,19 @@ int main()
     scanf("%d", &n);
     Estudante estudantes[n];
 
+    // Entrada de dados dos Alunos
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &estudantes[i].RGA);
         scanf(" %[^\n]", estudantes[i].nome);
         scanf("%lf", &estudantes[i].media);
     }
+    // Leitura da Operacao
     scanf(" %c", &op);
+    // Chamado do metodo responsavel por ordenar os alunos de acordo com operacao solicitada acima.
     ordenaEstudantes(estudantes, n, op);
 
+    // Leitura do numero de buscas a serem realizadas.
     scanf("%d", &qtdBuscas);
 
     for (int i = 0; i < qtdBuscas; i++)
